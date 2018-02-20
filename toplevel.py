7db18d0e2dee396f3f0460e19acda0bad294f751
Yi Zhang <yz3206@columbia.edu>
2018-02-20 15:28:39 -0500

    add evaluation in toplevel.py

--- toplevel.py
+++ toplevel.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 
 if __name__ == "__main__":
     os.system("ocamlbuild -clean")
@@ -7,6 +8,17 @@ if __name__ == "__main__":
 
     for i in range (1,6,1):
     	print ("\nPositive test " + str(i)+":")
-    	os.system("./toplevel.native tests/test_pos_"+str(i)+".of")
+    	output1 = os.popen("./toplevel.native tests/test_pos_"+str(i)+".of").read()
+    	if "it passes" in output1:
+    		print (output1.strip())
+    		print ("The result matches what we expected")
+    	else:
+    		print ("Wrong result")
+
     	print ("\nNegative test " + str(i)+":")
-    	os.system("./toplevel.native tests/test_neg_"+str(i)+".of")
+    	output2 = os.popen("./toplevel.native tests/test_neg_"+str(i)+".of").read()
+    	if "it passes" not in output2:
+    		print ("The result matches what we expected")
+    	else:
+    		print (output2.strip())
+    		print ("Wrong result")
