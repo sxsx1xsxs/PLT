e00a72fbe1552d82d0f54dcf314a3d505f86e91a
Yi Zhang <yz3206@columbia.edu>
2018-02-20 15:00:10 -0500

    change toplevel to python3 syntax

--- toplevel.py
+++ toplevel.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     os.system("ocamlbuild toplevel.native")
 
     for i in range (1,6,1):
-    	print "\nPositive test " + str(i)+":"
+    	print ("\nPositive test " + str(i)+":")
     	os.system("./toplevel.native tests/test_pos_"+str(i)+".of")
-    	print "\nNegative test " + str(i)+":"
+    	print ("\nNegative test " + str(i)+":")
     	os.system("./toplevel.native tests/test_neg_"+str(i)+".of")
