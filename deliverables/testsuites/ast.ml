185e7f0f96254aa7492d58240227537c45d6a898
Yi Zhang <yz3206@columbia.edu>
2018-04-19 01:05:13 -0400

    adding testsuites deliverable

---
+++ deliverables/testsuites/ast.ml
@@ -0,0 +1,131 @@
+(* AST.ml *)
+(* This code was original from MicroC. *)
+
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq | And | Or
+
+type uop = Neg | Not
+
+type typ = Int | Bool | Float | Void | String | Arr of typ * int
+
+type expr = Literal of int             | BoolLit of bool
+          | Fliteral of float          | Id of string
+          | Sliteral of string         | Binop of expr * op * expr 
+          | Unop of uop * expr         | Assign of expr * expr   
+          | Call of string * expr list | Noexpr
+          | Array_Index of string * expr
+          | Array_Lit of expr list
+
+
+type stmt = Block of stmt list 
+          | Expr of expr 
+          | Return of expr
+          | If of expr * stmt * stmt 
+          | While of expr * stmt
+          | For of expr * expr * expr * stmt
+
+
+(* type decl = var_decl list | func_decl list *)
+
+type var_decl = VarDecl of typ * string * expr
+
+type func_decl = {
+        ftyp    : typ;
+        fname   : string;
+        formals : var_decl list;
+        locals  : var_decl list;
+        body    : stmt list;
+}
+
+type pattern = RegexPattern of string
+
+type program = var_decl list * func_decl list
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | Leq -> "<="
+  | Greater -> ">"
+  | Geq -> ">="
+  | And -> "&&"
+  | Or -> "||"
+
+let string_of_uop = function
+    Neg -> "-"
+  | Not -> "!"
+
+
+(* let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+  | Float -> "float"
+  | Void -> "void"
+  | String -> "string" *)
+  
+let rec string_of_typ_ps = function
+    Int -> "int", ""
+  | Bool -> "bool", ""
+  | Void -> "void", ""
+  | Float -> "float", ""
+  | Arr(typ, len) ->
+    let pref, suf = string_of_typ_ps typ in
+    pref, "[" ^ (string_of_int len) ^ "]" ^ suf
+  | String -> "string", ""
+
+let string_of_typ t =
+  (* Split by prefix and suffix so array dims are in the right order *)
+  let p, s = string_of_typ_ps t in
+  p ^ s
+
+let rec string_of_expr = function
+    Literal(l) -> string_of_int l
+  | Fliteral(l) -> string_of_float l
+  | Sliteral(l) -> l
+  | BoolLit(true) -> "true"
+  | BoolLit(false) -> "false"
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign(v, e) -> string_of_expr v ^ " = " ^ string_of_expr e
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Noexpr -> ""
+  | Array_Lit l -> "[" ^ String.concat ", " (List.map string_of_expr l) ^ "]"
+  | Array_Index (e, ind) -> e ^ "[" ^ string_of_expr ind ^ "]"
+  
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_vdecl = function
+   VarDecl(t, id, Noexpr) -> string_of_typ t ^ " " ^ id
+  |VarDecl(t, id, e) -> string_of_typ t ^ " " ^ id ^ " = " ^ string_of_expr e
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.ftyp ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map string_of_vdecl fdecl.formals) ^
+  ")\n{\n" ^
+  String.concat ";\n" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_pattern x = "@" ^ x ^ "@"
+
+let string_of_program (vars, funcs) =
+  String.concat ";\n" (List.map string_of_vdecl vars) ^ ";\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
+
---
+++ deliverables/testsuites/sast.ml
@@ -0,0 +1,34 @@
+(* Semantically-checked Abstract Syntax Tree and functions for printing it *)
+
+open Ast
+
+type sexpr = typ * sx
+and sx = 
+	SLiteral of int               | SBoolLit of bool
+	| SFliteral of float          | SId of string
+	| SSliteral of string         | SBinop of sexpr * op * sexpr 
+	| SUnop of uop * sexpr         | SAssign of sexpr * sexpr   
+	| SCall of string * sexpr list | SNoexpr
+  | SArray_Index of string * expr
+  | SArray_Lit of expr list
+
+	
+type sstmt = 
+	SBlock of sstmt list | SExpr of sexpr | SReturn of sexpr
+          | SIf of sexpr * sstmt * sstmt | SWhile of sexpr * sstmt
+          | SFor of sexpr * sexpr * sexpr * sstmt
+		  
+type sinit = typ * string * sexpr
+
+type svar_decl = typ * string * sexpr
+
+type sfunc_decl = {
+        sftyp    : typ;
+        sfname   : string;
+        sformals : var_decl list;
+        slocals  : var_decl list;
+        sbody    : sstmt list;
+}
+
+
+type sprogram = svar_decl list * sfunc_decl list
